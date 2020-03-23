00a5001c4ebb2c876b10813007c4364acddfbfe8
toth12 <gabor.toth@yale.edu>
2020-03-23 16:27:37 -0700

    Interview lenght measured

--- data_analysis/analyze_segments_data.py
+++ data_analysis/analyze_segments_data.py
@@ -19,15 +19,15 @@ input_files = [input_directory+'/'+i for i in input_files]
 # Read the input files into panda dataframe
 
 csv_data = []
-for el in input_files[0:1]:
+for el in input_files:
     f = codecs.open(el,"rb","utf-8")
     csvread = csv.reader(f,delimiter=',')
     csv_data_temp = list(csvread)
     columns = csv_data_temp[0]
-    csv_data.extend(csv_data_temp[1:10])
+    csv_data.extend(csv_data_temp[1:])
 
 
-
+columns[0] = "IntCode"
 df = pd.DataFrame(csv_data,columns=columns)
 
 # Initiate an empty string to hold the report data
@@ -44,17 +44,54 @@ report += "Total number of datapoints: "+str(number_data_points)+".\n"
 
 # Calculate the length of time a person speaks about Auschwitz
 
-begining = df['InTimeCode'].iloc[0]
-end = df['OutTimeCode'].iloc[0]
+
+# Transform InTimeCode OutTimeCode into temporal data
+
+
 
 df['InTimeCode'] = pd.to_datetime(df['InTimeCode'], format = "%H:%M:%S:%f")
+
+
 df['OutTimeCode'] = pd.to_datetime(df['OutTimeCode'], format = "%H:%M:%S:%f")
 
-df['segment_lenght'] = df['OutTimeCode'] - df['InTimeCode']
+# Drop duplicated segments
 
-pd.to_datetime('00:15:00:00', format = "%H:%M:%S:%f")
 
-pd.to_datetime('00:15:03:00', format = "%H:%M:%S:%f").time()
+
+df_segment_length = df.drop_duplicates('SegmentID')
+
+# Eliminate those segments where the OutTapeNumber and the InTapenumber are not the same
+
+not_equal=len(df_segment_length[df_segment_length['OutTapenumber']!=df_segment_length['InTapenumber']])
+
+
+
+df_segment_length = df_segment_length[df_segment_length['OutTapenumber']==df_segment_length['InTapenumber']]
+
+
+
+# Calculate the segment length (not the ones above)
+df_segment_length['segment_lenght'] = df_segment_length['OutTimeCode'] - df_segment_length['InTimeCode']
+
+
+# Calculate how much time an interview speaks about Auschwitzy
+df_interview_segment_length = df_segment_length.groupby(['IntCode'])['segment_lenght'].agg('sum')
+
+df_interview_segment_length = pd.DataFrame({'IntCode':df_interview_segment_length.index, 'length':df_interview_segment_length.values})
+
+df_interview_segment_length = df_interview_segment_length.sort_values('length')
+df_interview_segment_length.to_csv('test_2.csv')
+
+pdb.set_trace()
+
+df_segment_length[df_segment_length['\ufeffIntCode']=='10']
+
+df[df['SegmentID']=='198988'].to_csv('test.csv')
+
+df[df['OutTapenumber']==df['InTapenumber']]
+
+pd.DataFrame(df_interview_segment_length)
+
 
 """FMT = '%H:%M:%S:%f'
 (Pdb) tdelta = datetime.strptime('00:15:00:00', FMT) - datetime.strptime('00:16:00:00', FMT)
