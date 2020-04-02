89546573bd430d62765b021dbfff48e4d4726a18
toth12 <gabor.toth@yale.edu>
2020-04-02 14:36:41 -0700

    Chi2 test working

--- data_analysis/chi2test.py
+++ data_analysis/chi2test.py
@@ -59,13 +59,41 @@ df = df[df['IntCode'].isin(IntCode)]
 
 df_biodata = df_biodata[df_biodata['IntCode'].isin(IntCode)]
 
-suicides = df[df.KeywordLabel.str.contains('trial witnesses')][['IntCode']].IntCode.unique().tolist()
+gender = df_biodata[['Gender','IntCode']]
+df["IntCode"] = df.IntCode.map(lambda x: int(x))
+df = df.merge(gender,how='left',on="IntCode")
+df = pd.concat([df, df.Gender.str.get_dummies()], axis=1)
+contingency = df.groupby(['KeywordID','KeywordLabel','IntCode']).agg({'F': 'sum', 'M': 'sum'}).reset_index()
 
-df_biodata['suicide_discussed'] = [True if str(x) in suicides else False for x in df_biodata['IntCode'] ]
-df_suicides = df_biodata.groupby(["Gender",'suicide_discussed'])[['IntCode']].count().reset_index()
+contingency['M'] = contingency['M'].apply(lambda x: 0 if x <1 else 1)
+contingency['F'] = contingency['F'].apply(lambda x: 0 if x <1 else 1)
+contingency = contingency.groupby(['KeywordID',"KeywordLabel"]).agg({'F': 'sum', 'M': 'sum'}).reset_index()
 
-obs = np.array([[df_suicides.iloc[0]['IntCode'], df_suicides.iloc[1]['IntCode']], [df_suicides.iloc[2]['IntCode'],df_suicides.iloc[3]['IntCode']]])
+total_f=len(df_biodata[df_biodata['Gender']=="F"])
+total_m=len(df_biodata[df_biodata['Gender']=="M"])
 
-chi2_contingency(obs)
+result = []
+for element in contingency.iterrows():
+
+    female = element[1]['F']
+    male = element[1]['M']
+    not_female = total_f - element[1]['F']
+    not_male = total_m - element[1]['M']
+    obs = np.array([[male,not_male],[female, not_female]])
+    test_stat = chi2_contingency(obs)[0]
+    res_female= obs[1][0]/chi2_contingency(obs)[3][1][0]
+    res_male= obs[0][0]/chi2_contingency(obs)[3][0][0]
+    result.append([element[1]['KeywordID'],element[1]['KeywordLabel'],res_female,res_male, test_stat])
+    
+
+
+
+df_chi = pd.DataFrame(result,columns=['KeywordID','KeywordLabel','female','male','test_stat'])
+df_chi = df_chi.sort_values('test_stat',ascending=False)
+
+
+#obs = np.array([[df_suicides.iloc[0]['IntCode'], df_suicides.iloc[1]['IntCode']], [df_suicides.iloc[2]['IntCode'],df_suicides.iloc[3]['IntCode']]])
+
+#chi2_contingency(obs)
 pdb.set_trace()
 
