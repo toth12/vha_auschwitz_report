da491d93715ec36b4c5b92ed70d3f680925817df
toth12 <gabor.toth@yale.edu>
2020-04-03 11:52:05 -0700

    Parsing tree working

--- feature_engineering/get_nodes_and_sub_nodes.py
+++ feature_engineering/get_nodes_and_sub_nodes.py
@@ -20,8 +20,54 @@ with codecs.open(input_directory+input_files_term_hierarchy,encoding = "utf-8-si
 
 root = importer.import_(treedict)
 
+
+conditions_container_nodes = ['camp living conditions','housing conditions','environmental conditions']
+clothing_container_nodes =['clothing']
+def find_container_subnodes(container_node_names):
+    node_ids = []
+    for element in conditions_container_nodes:
+        container_node = search.findall_by_attr(root,element,name='name')
+        if len(container_node) ==0:
+            print (element)
+        for node in container_node:
+            node_ids.append(node.id)
+            descendants = node.descendants
+            for descendant in descendants:
+                node_ids.append(descendant.id)
+    node_ids = set(node_ids)
+    node_ids = [i for i in node_ids]
+    return node_ids
+
+
+def find_nodes_by_name_pattern(name_pattern):
+    node_ids = []
+    
+    nodes = search.findall(root,filter_=lambda node: name_pattern in node.name)
+    for node in nodes:
+            node_ids.append(node.id)
+            descendants = node.descendants
+            for descendant in descendants:
+                node_ids.append(descendant.id)
+    node_ids = set(node_ids)
+    node_ids = [i for i in node_ids]
+    return node_ids
+
+conditions = find_container_subnodes(conditions_container_nodes)
+clothing = find_container_subnodes(clothing_container_nodes)
+shoes = find_nodes_by_name_pattern('shoes')
+loved_ones = find_nodes_by_name_pattern('loved ones')
+latrines = find_nodes_by_name_pattern('latrines')
+pdb.set_trace()
+
+#7. Wartime family interactions + family members
+# 10. Social relations + friendship + food sharing
+#11. Barter + covert economic activities
+#22. brutal treatment + executions and killings + violent attacks -> Violence 
+
+
+
 camp_living_conditions = search.findall_by_attr(root,'camp living conditions',name='name')[0].descendants
-housing_conditions = search.findall_by_attr(root,'housing Conditions',name='name')[0].descendants
+housing_conditions = search.findall_by_attr(root,'housing conditions',name='name')[0].descendants
 environment_conditions = search.findall_by_attr(root,'environment conditions',name='name')[0].descendants
 
 
