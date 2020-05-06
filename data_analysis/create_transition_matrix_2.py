5513743da14ba9478105f87743b074a9e99ac906
toth12 <gabor.toth@yale.edu>
2020-05-06 15:05:02 -0700

    Topic added

--- data_analysis/create_transition_matrix_2.py
+++ data_analysis/create_transition_matrix_2.py
@@ -5,10 +5,18 @@ import numpy as np
 from pyemma import msm,plots
 import msmtools
 from msmtools.flux import tpt,ReactiveFlux
+from pyemma import plots as mplt
 
 from scipy import sparse
 
 np.set_printoptions(suppress=True)
+
+
+topic_doc = open('data_analysis/topics_enumerated.txt').read()
+
+topic_labels = [element.split('\n')[0].strip() for element in topic_doc.split('\n\n')]
+
+
 
 
 def window(seq, n=2):
@@ -37,6 +45,8 @@ topic_list=data.topic.unique().tolist()
 topic_list.remove('unknown_topic')
 topic_list = sorted(topic_list)
 
+
+
 transition_matrix = np.zeros([len(topic_list),len(topic_list)]).astype(int)
 
 for element in transitions:
@@ -47,17 +57,31 @@ for element in transitions:
         state2_index= topic_list.index(state2)
         transition_matrix[state1_index,state2_index] = transition_matrix[state1_index,state2_index] + 1
 
+
+
 to_be_removed=np.sort(np.where(np.all(transition_matrix == 0, axis=1)==True))[0][::-1]
 for element in to_be_removed:
     transition_matrix=np.delete(transition_matrix,element,axis=1)
     transition_matrix=np.delete(transition_matrix,element,axis=0)
     del topic_list[element]
 
+
+topic_list_with_labels=[]
+for element in topic_list:
+    topic_n = element.split('_')[1:]
+    labels = '_'.join([topic_labels[int(l)] for l in topic_n])
+    topic_list_with_labels.append(labels)
+
+
+
+
+
+
 transition_matrix_scaled = (transition_matrix.T/transition_matrix.sum(axis=1)).T
 
 
 #transition_matrix_scaled  = sparse.csr_matrix(transition_matrix_scaled)
-pdb.set_trace()
+
 mm = msm.markov_model(transition_matrix_scaled)
 
 #print the stationary distribution
@@ -77,27 +101,57 @@ A = [topic_list.index('topic_2')]
 B = [topic_list.index('topic_8_15')]
 tpt = msm.tpt(mm, A, B)
 
+nCut = 1
 (bestpaths,bestpathfluxes) = tpt.pathways(fraction=0.3)
 cumflux = 0
+
+all_stations = [element for item in bestpaths for element in item]
+
+all_stations.sort()
+
+all_stations=set(all_stations)
+
+all_connections = np.zeros((len(all_stations),len(all_stations)))
+
+
+
 print("Path flux\t\t%path\t%of total\tpath")
 for i in range(len(bestpaths)):
     cumflux += bestpathfluxes[i]
+
     print(bestpathfluxes[i],'\t','%3.1f'%(100.0*bestpathfluxes[i]/tpt.total_flux),'%\t','%3.1f'%(100.0*cumflux/tpt.total_flux),'%\t\t',bestpaths[i])
+    
+    for element in bestpaths[i]:
+        print (topic_list_with_labels[element])
+    #get the path labels
 
 
 
-
+    #inbetween= bestpaths[i][nCut:len(bestpaths[i])-nCut]
 pdb.set_trace()
+
+
+
+
+
+
 print('Total TPT flux = ', tpt.total_flux)
 print('Rate from TPT flux = ', tpt.rate)
 print('A->B transition time = ', 1.0/tpt.rate)
 
 print('mfpt(0,4) = ', mm.mfpt(0, 4))
 
+Fsub = tpt.major_flux(fraction=0.1)
+print(Fsub)
+Fsubpercent = 100.0 * Fsub / tpt.total_flux
+plt = mplt.plot_network(Fsubpercent, state_sizes=tpt.stationary_distribution, arrow_label_format="%3.1f")
+
 
 pdb.set_trace()
 
 
+
+# Find the eigenvalues
 
 
 for element in mm.eigenvalues().argsort()[::-1]:
