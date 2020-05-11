09a4820ffb07761f70322e08ca24c23384af190b
toth12 <gabor.toth@yale.edu>
2020-05-11 15:56:52 -0700

    Key files ready

--- README.md
+++ README.md
@@ -35,15 +35,15 @@ pip install -r requirements.txt
 * Topic modelling of the data set
 * Markov Chain analysis of topic sequences in the data set
 
-## Topic modelling of the data set:
+## Markov Chain analysis of topic sequences in the data set:
 
-This workflow extracts the key topics underlying the segment data. For topic modelling, it uses the Corex topic modelling alghorithms. The workflow consists of the following sub-steps
+This workflow first transforms the segment data into a segment-index term matrix. Next it accomplishes an anchored topic modelling over the segment-index term matrix and gives a topic label to each segment. As a result, each interview is represented not only as a sequence of segments, but as a sequence of topics. As a last step, the workflow creates a transition matrix from the topic sequences (each topic is a state in the transition matrix), and trains a Markov model from the transition matrix.
 
 ### Create a three segments - index terms matrix:
 
-Run the following code (takes a few minutes):
+Run the following code (takes a few minutes) from the main project folder (use python3):
 ```
-python feature_engineering/create_grouped_segment_keyword_matrix.py
+python3 feature_engineering/create_grouped_segment_keyword_matrix.py
 ```
 
 Input data:
@@ -68,7 +68,50 @@ This script contains the following key steps and resulting representations:
 * eliminates those interviews where no group of three segments could be identified
 * eliminates those index terms that occur in less than 50 interviews in total
 * creates a numpy segment - index matrix; every row represents a new segment unit, and every column represents an index term
-* creates a lookup index for the segment - index matrix; segment_index_merged.csv is a panda dataframe where every row has an "updated_id", which is the new segment id of the corresponding row in the segment - index matrix; keyword_index_merged_segments.csv is a panda dataframe, every row (with keyword id and keyword label) corresponds to the columns of segment - index matrix
+* creates a lookup index for the segment - index matrix; segment_index_merged.csv is a panda dataframe where every row has an "updated_id", which is the new segment id (see above) of the corresponding row in the segment - index matrix; keyword_index_merged_segments.csv is a panda dataframe, every row (with keyword id and keyword label) corresponds to the columns of segment - index matrix
+
+### Give a topic label to each group of three segments:
+
+
+Run the following code from the main project folder (use python3):
+```
+python3 data_analysis/add_topic_label_to_segments.py
+```
+
+Input data:
+
+* 'data/output/features/segment_keyword_matrix_merged.txt'
+* 'data/output/features/keyword_index_merged_segments.csv'
+* 'data/output/features/segment_index_merged.csv'
+* 'data_analysis/topic_anchors.txt'
+
+Output data:
+
+* 'data/output/topic_sequencing/segment_topics.csv'
+
+This script contains the following key steps:
+* accomplishes the anchored (sometimes called seeded) topic modelling with Corex over the previously constructed segment-topic matrix; topic seeds were identified earlier and they are in data_analysis/topic_anchors.txt
+* produces a segment topic matrix (each topic is one of topic seeds above);
+* adds a topic label to each segment; since each segment can have multiple topics, topic labels can be also the combinations of single topics or if no topic can be identified, their topic label is "unknown topic"
+* saves the topic label of each segment in a panda dataframe (segment_topics.csv)
+
+
+### Create a transition matrix between each topics and train a markov chain:
+
+Run the following code from the main project folder (use python3):
+```
+python3 create_transition_matrix_and_train_markov_chain.py
+```
+
+
+Input data:
+* 'data/output/topic_sequencing/segment_topics.csv'
+* 'data/input/biodata.xlsx'
+
+This script contains the following key steps:
+* creates a transition matrix between topics for men and women but ignores transition from unknown_topic or to unknown_topic
+* removes those topics to which or from which no transition takes place
+* prints the best path between two key topics: selection and camp_liquidation / transfer
 
 
 
---
+++ data_analysis/add_topic_label_to_segments.py
@@ -0,0 +1,150 @@
+import numpy as np
+import scipy.sparse as ss
+from corextopic import corextopic as ct
+import pandas as pd
+import pdb
+from corextopic import vis_topic as vt
+import constants
+import codecs
+import json
+import prince
+
+# Read the input data
+
+input_directory = constants.output_data_features
+
+# Read the segment index term matrix
+data = np.loadtxt(input_directory+'segment_keyword_matrix_merged.txt', dtype=int)
+
+# Read the column index (index terms) of the matrix above
+features_df = pd.read_csv(input_directory+'keyword_index_merged_segments.csv')
+
+# Read the row index (groups of three segments) of the matrix above
+segment_df = pd.read_csv(input_directory+'segment_index_merged.csv')
+
+# Get the index label
+features = features_df['KeywordLabel'].values.tolist()
+
+# Open the topics for the anchored topic modelling
+with codecs.open("data_analysis/topic_anchors.txt") as topics_file:
+        topics = topics_file.read()
+
+topic_list = topics.split('\n\n')
+
+anchors = []
+
+# Check if topic word certainly exists
+for topic in topic_list:
+    anchor = []
+    topic_words = topic.split('\n')
+    for topic_word in topic_words:
+        if not topic_word.strip() in features_df['KeywordLabel'].tolist():
+            pdb.set_trace()
+        else:
+            anchor.append(topic_word.strip())
+    anchors.append(anchor)
+
+
+
+# Create a sparse matrix from the numpy segment index term matrix
+
+X = ss.csr_matrix(data)
+
+
+
+# Train the CorEx topic model
+topic_model = ct.Corex(n_hidden=len(anchors))  # Define the number of latent (hidden) topics to use.
+
+
+topic_model.fit(X, docs=segment_df.updated_id.tolist(),words=features,anchors=anchors, anchor_strength=10)
+
+topics = topic_model.get_topics()
+
+# Print the key topics
+
+for topic_n,topic in enumerate(topics):
+    words,mis = zip(*topic)
+    topic_str = str(topic_n)+': '+','.join(words)
+    print(topic_str)
+
+# Print the documents most relevant for each topic
+top_docs = topic_model.get_top_docs()
+for topic_n, topic_docs in enumerate(top_docs):
+    docs,probs = zip(*topic_docs)
+    docs = [str(element)for element in docs] 
+    topic_str = str(topic_n+1)+': '+','.join(docs)
+    print(topic_str)
+
+# Get the document topic matrix
+document_topic_matrix = topic_model.labels.astype(int)
+# Helper functions below to find out if there documents that are labeled with a given number of topics
+
+#p.where(document_topic_matrix.sum(1)==12)[0].shape
+#print (np.where(document_topic_matrix.sum(1)==0)[0].shape)
+
+
+# Find all possible topic combinations (a document can have multiple topics) and how many times they occurs
+(unique, counts) = np.unique(document_topic_matrix,axis=0, return_counts=True)
+
+
+
+
+# Create new topics that are the combinations of multiple single topics
+
+# Create an empty list that will hold these new topics
+new_topics = []
+
+# Iterate through all possible topic combinations (each is numpy array)
+for i,element in enumerate(unique):
+    # Find out how many individual topics there are in each topic combination
+    nonzeros = np.count_nonzero(element)
+
+    # Topic combinations that consists of only one topic is one of the anchored topic above, no need to create a new topic from
+    if nonzeros >1:
+        # Count how many instances this topic combination has in the data
+        # If occurs more than 50 times, add it to the list of new topics
+        if counts[i] >50:
+            # Every topic is to be named with a prefix (topic_) followed by all topics that give the topic combination
+            new_topic = 'topic_'+'_'.join([str(num) for num in np.nonzero(element)[0].tolist()])
+            new_topics.append(new_topic)
+
+# With the new topics (in fact topic combinations), as well as with the anchored topics, define the topic label of each segment
+
+# To follow how many topics have unknown topic (see below what we mean by unknown topic make a counter)
+count_segment_without_topic = 0
+
+segment_topics = []
+# Iterate through the document topic matrix
+for i,element in enumerate(document_topic_matrix):
+    # Find the topics of it
+    nonzeros = np.count_nonzero(element)
+
+    # If it has more than one topic label
+    if nonzeros > 1:
+        # Create the topic label
+        topic = 'topic_'+'_'.join([str(num) for num in np.nonzero(element)[0].tolist()])
+        # If the topic label does not exist (count of topic combination is below the threshold above), label the segment with unknown topic
+        if (topic not in new_topics):
+            count_segment_without_topic = count_segment_without_topic+1
+            segment_topics.append("unknown_topic")
+        # Otherwise label it with the topic combination
+        else:
+            segment_topics.append(topic)
+    elif nonzeros ==1:
+        topic = 'topic_'+str(np.nonzero(element)[0].tolist()[0])
+        segment_topics.append(topic)
+    # If zero topic was added to a segment label it with unknown topic
+    else:       
+        segment_topics.append("unknown_topic")
+
+
+# Update the segment dataframe and add the topic label to it
+# For instance: the topic of 10006_47_48_49 (interview code 10006, segments 47,48,49) is topic_2
+#        Unnamed: 0      updated_id                                          KeywordID          topic
+ #   0               3  10006_47_48_49                          ['10983' '12044' '14280']        topic_2
+segment_df['topic'] = segment_topics
+
+
+
+# Save the dataframe 
+segment_df.to_csv('data/output/topic_sequencing/segment_topics.csv')
