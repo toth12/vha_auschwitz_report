6dca4792eff629a664345b41af68069b12cf35a4
thempel <thempel@zedat.fu-berlin.de>
2020-08-19 11:34:00 +0200

    fix nested data struct with dict, use pyemma

--- markov_modelling/train_markov_model.py
+++ markov_modelling/train_markov_model.py
@@ -6,75 +6,86 @@ import os
 from msmtools.estimation import is_connected
 from markov_utils import train_markov_chain,window,cg_transition_matrix,train_markov_chain,print_stationary_distributions,post_process_topic_sequences
 import sys
-
+import pyemma
+from tqdm.auto import tqdm
 
 def prepare_input_data(metadata_field):
     # Find the relevant interview ids
     interview_codes = []
     metadata_field_names = []
     if "_w" in metadata_field:
-            interview_codes_to_filter=IntCodeW
+        interview_codes_to_filter = IntCodeW
     elif "_m" in metadata_field:
-            interview_codes_to_filter=IntCodeM
+        interview_codes_to_filter = IntCodeM
     else:
-            interview_codes_to_filter =[]
-    
+        interview_codes_to_filter = []
+
     if "complete" in metadata_field:
         interview_codes_temp = df_biodata.IntCode.to_list()
 
-        if len(interview_codes_to_filter)>1:
+        if len(interview_codes_to_filter) > 1:
             interview_codes_temp = [f for f in interview_codes_temp if f in interview_codes_to_filter]
-        interview_codes += interview_codes_temp
-        metadata_field_names += metadata_field
+        interview_codes.append(interview_codes_temp)
+        metadata_field_names.append(metadata_field)
 
     elif (('easy' in metadata_field) or ('medium' in metadata_field) or ('hard' in metadata_field)):
-        interview_codes_temp = df_biodata[df_biodata[metadata_field.split('_')[0]]==1].IntCode.tolist()
+        interview_codes_temp = df_biodata[df_biodata[metadata_field.split('_')[0]] == 1].IntCode.tolist()
 
-        if len(interview_codes_to_filter)>1:
+        if len(interview_codes_to_filter) > 1:
             interview_codes_temp = [f for f in interview_codes_temp if f in interview_codes_to_filter]
-        interview_codes += interview_codes_temp
-        metadata_field_names += metadata_field
+        interview_codes.append(interview_codes_temp)
+        metadata_field_names.append(metadata_field)
     elif "notwork" in metadata_field:
-        interview_codes_temp = df_biodata[(df_biodata['easy']==0)&(df_biodata['hard']==0)&(df_biodata['medium']==0)].IntCode.tolist()
-        if len(interview_codes_to_filter)>1:
+        interview_codes_temp = df_biodata[
+            (df_biodata['easy'] == 0) & (df_biodata['hard'] == 0) & (df_biodata['medium'] == 0)].IntCode.tolist()
+        if len(interview_codes_to_filter) > 1:
             interview_codes_temp = [f for f in interview_codes_temp if f in interview_codes_to_filter]
-        interview_codes += interview_codes_temp
-        metadata_field_names += metadata_field
+        interview_codes.append(interview_codes_temp)
+        metadata_field_names.append(metadata_field)
     elif "work" in metadata_field:
-        interview_codes_temp = df_biodata[(df_biodata['easy']==1)|(df_biodata['hard']==1)|(df_biodata['medium']==1)].IntCode.tolist()
-        if len(interview_codes_to_filter)>1:
+        interview_codes_temp = df_biodata[
+            (df_biodata['easy'] == 1) | (df_biodata['hard'] == 1) | (df_biodata['medium'] == 1)].IntCode.tolist()
+        if len(interview_codes_to_filter) > 1:
             interview_codes_temp = [f for f in interview_codes_temp if f in interview_codes_to_filter]
-        interview_codes += interview_codes_temp
-        metadata_field_names += metadata_field
+        interview_codes.append(interview_codes_temp)
+        metadata_field_names.append(metadata_field)
     elif "CountryOfBirth" in metadata_field:
-        country_of_origins = df_biodata.groupby('CountryOfBirth')['CountryOfBirth'].count().to_frame('Count').reset_index()
-        country_of_origins= country_of_origins[country_of_origins.Count>50]
+        country_of_origins = df_biodata.groupby('CountryOfBirth')['CountryOfBirth'].count().to_frame(
+            'Count').reset_index()
+        country_of_origins = country_of_origins[country_of_origins.Count > 50]
         country_of_origins = country_of_origins.CountryOfBirth.tolist()
         for el in country_of_origins:
-            interview_codes_temp = df_biodata[df_biodata['CountryOfBirth']==el].IntCode.to_list()
-            if len(interview_codes_to_filter)>1:
+            interview_codes_temp = df_biodata[df_biodata['CountryOfBirth'] == el].IntCode.to_list()
+            if len(interview_codes_to_filter) > 1:
                 interview_codes_temp = [f for f in interview_codes_temp if f in interview_codes_to_filter]
                 if "_w" in metadata_field:
-                    el = el+'_w'
+                    el = el + '_w'
                 else:
-                    el = el+'_m'
-            interview_codes += interview_codes_temp
-            metadata_field_names += el
+                    el = el + '_m'
+            interview_codes.append(interview_codes_temp)
+            metadata_field_names.append(el)
 
-    output_data = []
-    segment_indices = []
+    output_data = {}
+    segment_indices = {}
 
-    assert len(interview_codes) > 1
+    print(len(interview_codes), len(interview_codes[0]))
+    # print(interview_codes)
 
-    for element in interview_codes:
-        segment_index = segment_df[segment_df.IntCode.isin([element])].index.to_list()
-        input_matrix = np.take(data, segment_index, axis=0)
-        if input_matrix.size == 0:
-            continue
-        output_data.append(input_matrix)
-        segment_indices.append(segment_df[segment_df.IntCode.isin([element])])
+    for sublist, fieldname in zip(interview_codes, metadata_field_names):
+        output_data[fieldname] = []
+        segment_indices[fieldname] = []
+        for element in tqdm(sublist):
+            # print(type(element), element)
+            segment_index = segment_df[segment_df.IntCode.isin([element])].index.to_list()
+            input_matrix = np.take(data, segment_index, axis=0)
+            if input_matrix.size == 0:
+                continue
+
+            output_data[fieldname].append(input_matrix)
+            segment_indices[fieldname].append(segment_df[segment_df.IntCode.isin([element])])
 
     return output_data, metadata_field_names, segment_indices
+
 
 if __name__ == '__main__':
 
@@ -118,73 +129,38 @@ if __name__ == '__main__':
     for metadata_field in metadata_fields:
 
         # Prepare the input data (trajectories)
-        input_data_sets,metadata_field_names,segment_indices=prepare_input_data(metadata_field)
+        input_data_sets_dict, metadata_field_names, segment_indices_dict = prepare_input_data(metadata_field)
 
-        for f,input_data_set in enumerate(input_data_sets):
-
-            try:
-                
-                document_index = segment_indices[f].groupby(['IntCode','SegmentNumber'])['KeywordLabel'].apply(list).to_frame('KeywordSequence').reset_index()
-                print (metadata_field_names[f])
+        for metadata_field_name in metadata_field_names:
 
 
-                
-                unique = np.unique(input_data_set,axis=0)
-                trajectories = []
-                for i,element in enumerate(input_data_set):
+            document_index = segment_indices_dict[metadata_field_name].groupby(['IntCode','SegmentNumber'])['KeywordLabel'].apply(list).to_frame('KeywordSequence').reset_index()
+            print(metadata_field_name)
 
-                    trajectory = np.where(np.all(unique==element,axis=1))[0][0]
-                    trajectories.append(trajectory)
-            
-                tr = [el for el in window(trajectories)]
-                count_matrix = np.zeros((unique.shape[0],unique.shape[0])).astype(float)
+            trajectories = []
+            for input_data_set in input_data_sets_dict[metadata_field_name]:
 
-                
+                # make sure this is binary assignment
+                assert np.unique(np.sum(input_data_set, axis=1)) == np.array([1]), 'not one-hot encoded'
 
-                for element in tr:
+                # convert one-hot encoding to state definitions
+                _t = np.argmax(input_data_set, axis=1)
+                trajectories.append(_t)
 
-                    count_matrix[element[0],element[1]]=count_matrix[element[0],element[1]]+float(1)
+            #TODO: validate choice of lag time
+            mm = pyemma.msm.estimate_markov_model(trajectories, 1, reversible=False)
+            transition_matrix = mm.transition_matrix
 
+            print('active set fraction: ', mm.active_state_fraction)
 
-                count_matrix = count_matrix +1e-12
-                transition_matrix = (count_matrix / count_matrix.sum(axis=1,keepdims=1))
-                assert np.allclose(transition_matrix.sum(axis=1), 1)
-                assert msmtools.analysis.is_transition_matrix(transition_matrix)
-                assert is_connected(transition_matrix)
+            #TODO: re-write to take care of active set < full set
+            stationary_prob = print_stationary_distributions(mm, features_df.KeywordLabel.to_list())
+            pd.DataFrame(stationary_prob).to_csv(output_directory+metadata_field_name+'/stationary_probs.csv')
 
+            if os.path.exists(output_directory+metadata_field_name+'/pyemma_model'):
+                os.remove(output_directory+metadata_field_name+'/pyemma_model')
 
-                binary_map = (unique / unique.sum(axis=1,keepdims=1))
-                new_tra = cg_transition_matrix(transition_matrix,binary_map)
-                new_tra[np.isnan(new_tra)] = 0 # TODO: why nans?
-                new_tra = new_tra+1e-12
-                new_tra= (new_tra / new_tra.sum(axis=1,keepdims=1)) #TODO: not clear why -> only valid to compensate machine error
-                assert np.allclose(new_tra.sum(axis=1), 1)
-                assert msmtools.analysis.is_transition_matrix(new_tra)
-                assert is_connected(new_tra)
+            mm.save(output_directory+metadata_field_name+'/pyemma_model', 'simple',overwrite=True)
 
-                try:
-                    os.mkdir(output_directory+metadata_field_names[f])
-                except:
-                    pass
-
-                mm = train_markov_chain(new_tra)
-
-                stationary_prob = print_stationary_distributions(mm,features_df.KeywordLabel.to_list())
-                pd.DataFrame(stationary_prob).to_csv(output_directory+metadata_field_names[f]+'/stationary_probs.csv')
-
-                if os.path.exists(output_directory+metadata_field_names[f]+'/pyemma_model'):
-                    os.remove(output_directory+metadata_field_names[f]+'/pyemma_model')
-               
-                mm.save(output_directory+metadata_field_names[f]+'/pyemma_model', 'simple',overwrite=True)
-                
-                document_index.to_csv(output_directory+metadata_field_names[f]+'/document_index.csv')
-                np.savetxt(output_directory+metadata_field_names[f]+'/transition_matrix.np', new_tra, fmt='%.8f')
-           
-            except ValueError:
-                error_type, error_instance, traceback = sys.exc_info()
-                error_instance.args = (error_instance.args[0] + ' <modification>',)
-                
-                print ("The transition matrix could not be made in case of the folllowing metadata field:")
-                print (metadata_field_names[f]) 
-                
-                continue
+            document_index.to_csv(output_directory+metadata_field_name+'/document_index.csv')
+            np.savetxt(output_directory+metadata_field_name+'/transition_matrix.np', mm.transition_matrix, fmt='%.8f')
