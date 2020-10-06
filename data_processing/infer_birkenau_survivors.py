ec2bbca660921c853233768470fbe07e955f1938
toth12 <gabor.toth@yale.edu>
2020-10-06 14:48:26 +0200

    infer_birkenau_survivors.py updated only those interviews that made according to the new segmentation systsem

--- data_processing/infer_birkenau_survivors.py
+++ data_processing/infer_birkenau_survivors.py
@@ -19,6 +19,6 @@ output_file = constants.input_files_biodata_birkenau
 df_biodata = pd.read_csv(input_folder+input_file)
 df_biodata = df_biodata.fillna(0)
 
-df_biodata = df_biodata[((df_biodata.Birkenau_segment_percentage>0.7)&(df_biodata.earliest_year>1942)&(df_biodata.is_transfer_route==False))]
+df_biodata = df_biodata[((df_biodata.Birkenau_segment_percentage>0.7)&(df_biodata.earliest_year>1942)&(df_biodata.is_transfer_route==False)&(df_biodata.segmentation_system=='new'))]
 
 df_biodata.to_csv(output_folder+output_file)
