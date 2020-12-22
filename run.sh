233c0da51b0fd82bef1a2f069dd7bf3535a7175b
toth12 <gabor.toth@yale.edu>
2020-12-22 07:36:12 +0100

    Code cleaned and run.sh updated

--- run.sh
+++ run.sh
@@ -7,3 +7,4 @@ python data_processing/infer_birkenau_survivors.py
 python data_processing/partition_interviews_into_metadata_groups.py
 python statistical_analysis/measure_strength_of_assoc_odds_ratio.py
 python markov_modelling/train_markov_model.py
+python markov_modelling/create_bootstrapping_plots.py --metadata_fields complete_m complete_w
