b9dc93f45549ebb577dcb57b4917d584aa74b53d
toth12 <gabor.toth@yale.edu>
2021-01-05 14:22:02 +0100

    Data analysis continued

--- run.sh
+++ run.sh
@@ -5,7 +5,9 @@ python data_processing/create_segment_keyword_matrix.py
 python data_processing/map_indices_to_cover_terms.py
 python data_processing/infer_birkenau_survivors.py
 python data_processing/partition_interviews_into_metadata_groups.py
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py
+python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields complete_w complete_m
+python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields notwork_w notwork_m
 python markov_modelling/train_markov_model.py
 python markov_modelling/create_bootstrapping_plots.py --metadata_fields complete_m complete_w
 #python markov_modelling/create_bootstrapping_plots.py --metadata_fields work notwork
+#python markov_modelling/create_bootstrapping_plots.py --metadata_fields work_w work_m
