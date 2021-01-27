2f77f3fc0dcb2097a94724b8604069b63e02c4bd
toth12 <gabor.toth@yale.edu>
2021-01-27 08:12:40 +0100

    bootstrap updated

--- markov_modelling/create_bootstrapping_plots.py
+++ markov_modelling/create_bootstrapping_plots.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
         os.mkdir(output_directory)
     except:
         pass
-    ntrails = 50
+    ntrails = 1000
     for key in metadata_fields_to_agregate:
         indices = metadata_partitions[key]
 
--- run_temp.sh
+++ run_temp.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields complete_w complete_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields notwork_w notwork_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields work notwork
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields work_m work_w
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields notwork_w notwork_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Poland_w Poland_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Hungary_w Hungary_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Romania_w Romania_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Russia_w Russia_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Germany_w Germany_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Greece_w Greece_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Yugoslavia\ \(historical\)_w Yugoslavia\ \(historical\)_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Netherlands_w Netherlands_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields France_w France_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Austria_w Austria_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Italy_w Italy_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Austria-Hungary\ \(historical\)_w Austria-Hungary\ \(historical\)_m
-python statistical_analysis/measure_strength_of_assoc_odds_ratio.py --metadata_fields Czechoslovakia\ \(historical\)_w Czechoslovakia\ \(historical\)_m
-python markov_modelling/train_markov_model.py
 
 python markov_modelling/create_bootstrapping_plots.py --metadata_fields complete_m complete_w
 python markov_modelling/create_bootstrapping_plots.py --metadata_fields work notwork
